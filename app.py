3ce31dcf855e76b900937be77c126aada5963037
wahyudwi <86935386+wahyudwi99@users.noreply.github.com>
2023-02-17 17:25:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
