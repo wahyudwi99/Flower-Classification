3ebca70dbb398f87d538a88b308283f5676ed0a6
wahyudwi <86935386+wahyudwi99@users.noreply.github.com>
2023-03-21 14:45:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from PIL import Image
 from keras.utils import load_img, img_to_array
 import io
 import time
-from ultralytics import YOLO
+# from ultralytics import YOLO
 
 
 st.title('Flower Image Recognition')
@@ -28,69 +28,69 @@ if test != "":
     image_file = st.file_uploader('Upload flower image  here')
 
 
-    # if image_file is not None:
-    #     path = image_file.getvalue()
-    #     st.image(path)
-    #     open_image = Image.open(io.BytesIO(path))
-    #     img = tf.image.resize(open_image, [200,200])
-    #     img_array = img_to_array(img)
-    #     img_array = img_array/255
-
-    #     img_array = img_array.reshape(1,200,200,3)
-
-        # if st.button('Predict'):
-        #     model = load_model('Flower_model.h5')
-
-        #     prediction = model.predict(img_array)
-
-        #     with st.spinner('Classifying ...'):
-        #         time.sleep(3)
-
-        #     if (prediction[0][0] > prediction[0][1]) & (prediction[0][0] > prediction[0][2]):
-        #         prediction_daisy = np.round(prediction[0][0]*100, decimals=1)
-        #         st.write('This flower is ', str(prediction_daisy), '%', ' daisy')
-
-
-        #     elif (prediction[0][1] > prediction[0][0]) & (prediction[0][1] > prediction[0][2]):
-        #         prediction_sunflower = np.round(prediction[0][1]*100, decimals=1)
-        #         st.write('This flower is ', str(prediction_sunflower), '%', ' sunflower')
-
-
-        #     elif (prediction[0][2] > prediction[0][0]) & (prediction[0][2] > prediction[0][1]):
-        #         prediction_tulip = np.round(prediction[0][2]*100, decimals=1)
-        #         st.write('This flower is ', str(prediction_tulip), '%', ' tulip')
-
-
-        # else:
-        #     st.write('')
-
-
-
-    from numpy import asarray
-
-
     if image_file is not None:
         path = image_file.getvalue()
         st.image(path)
         open_image = Image.open(io.BytesIO(path))
-        img = tf.image.resize(open_image, [640,640])
-        img_array = asarray(img)
-        # img_array = img_array/255
+        img = tf.image.resize(open_image, [200,200])
+        img_array = img_to_array(img)
+        img_array = img_array/255
 
-        # img_array = img_array.reshape(1,200,200,3)
-
+        img_array = img_array.reshape(1,200,200,3)
 
         if st.button('Predict'):
-            model = YOLO('Medium_96_ACC.onnx')
+            model = load_model('Flower_model.h5')
 
             prediction = model.predict(img_array)
 
             with st.spinner('Classifying ...'):
                 time.sleep(3)
-            print(prediction[0].plot().shape)
-            print(type(prediction[0].plot()))
-            st.image(prediction[0].plot()/255, clamp=True)
+
+            if (prediction[0][0] > prediction[0][1]) & (prediction[0][0] > prediction[0][2]):
+                prediction_daisy = np.round(prediction[0][0]*100, decimals=1)
+                st.write('This flower is ', str(prediction_daisy), '%', ' daisy')
+
+
+            elif (prediction[0][1] > prediction[0][0]) & (prediction[0][1] > prediction[0][2]):
+                prediction_sunflower = np.round(prediction[0][1]*100, decimals=1)
+                st.write('This flower is ', str(prediction_sunflower), '%', ' sunflower')
+
+
+            elif (prediction[0][2] > prediction[0][0]) & (prediction[0][2] > prediction[0][1]):
+                prediction_tulip = np.round(prediction[0][2]*100, decimals=1)
+                st.write('This flower is ', str(prediction_tulip), '%', ' tulip')
 
 
         else:
             st.write('')
+
+
+# This is model for object detection using YOLO
+#     from numpy import asarray
+
+
+#     if image_file is not None:
+#         path = image_file.getvalue()
+#         st.image(path)
+#         open_image = Image.open(io.BytesIO(path))
+#         img = tf.image.resize(open_image, [640,640])
+#         img_array = asarray(img)
+#         # img_array = img_array/255
+
+#         # img_array = img_array.reshape(1,200,200,3)
+
+
+#         if st.button('Predict'):
+#             model = YOLO('Medium_96_ACC.onnx')
+
+#             prediction = model.predict(img_array)
+
+#             with st.spinner('Classifying ...'):
+#                 time.sleep(3)
+#             print(prediction[0].plot().shape)
+#             print(type(prediction[0].plot()))
+#             st.image(prediction[0].plot()/255, clamp=True)
+
+
+#         else:
+#             st.write('')
